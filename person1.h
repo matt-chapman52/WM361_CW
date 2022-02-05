6d89e0c3819b6f9c0cf4c645a1e748a9c4c0a332
Matt Chapman <mat.chapman66@me.com>
2022-02-05 18:55:28 +0000

    Added exmaple class layout

---
+++ person1.h
@@ -0,0 +1,11 @@
+/**
+ * @file person1.h
+ * @author your name (you@domain.com)
+ * @brief
+ *
+ */
+
+#ifndef person_1_H
+#define person_1_H
+
+#endif
--- person_1.cpp
+++ person_1.cpp
@@ -1,4 +1,12 @@
+/**
+ * @file person_1.cpp
+ * @author your name (you@domain.com)
+ * @brief
+ *
+ */
+
 #include <iostream>
+#include "person1.h"
 
 int main()
 {

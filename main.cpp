60964f914dd5ea9c57d5408c838b3e0d57804d59
Matt Chapman <mat.chapman66@me.com>
2022-02-23 19:21:32 +0000

    Continue options until exit

--- main.cpp
+++ main.cpp
@@ -14,26 +14,34 @@ int main()
     int option;
     // ITS it;
     Employee emp;
-    option = emp.showOptions();
 
-    switch (option)
+    while (true)
     {
-    case 1:
-        emp.getPersonalDetails();
-        break;
-    case 2:
-        emp.changePersonalDetails();
-        break;
-    case 3:
-        emp.requestLeave();
-        break;
-    case 4:
-        emp.viewLeave();
-        break;
-    case 5:
-        emp.changeLeave();
-        break;
-        // Case 6 to do, get the job role and show options
+        option = emp.showOptions();
+        switch (option)
+        {
+        case 1:
+            emp.getPersonalDetails();
+            break;
+        case 2:
+            emp.changePersonalDetails();
+            break;
+        case 3:
+            emp.requestLeave();
+            break;
+        case 4:
+            emp.viewLeave();
+            break;
+        case 5:
+            emp.changeLeave();
+            break;
+            // Case 6 to do, get the job role and show options
+        // case 6:
+        // if statement;
+        // break;
+        case 7:
+            exit(0);
+        }
     }
 
     // emp.getPersonalDetails();

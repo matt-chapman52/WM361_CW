1bc9dd7a71901c4c3074377d729ab62abbfcc538
pangja <jacktpang@gmail.com>
2022-02-23 14:12:00 +0000

    Add to Login.cpp and login.h

--- Login.cpp
+++ Login.cpp
@@ -4,3 +4,57 @@
  * @brief
  */
 
+#include "Login.h"
+
+// Constructor
+Login::Login()
+{
+    cout << "\n----- Welcome -----"
+         << "\nLogin to Continue"
+         << "\nPlease enter your employee number" << endl;
+    cin >> loginEmployeeNum;
+    VerifyEmployee();
+}
+
+Login::VerifyEmployee()
+{
+    int rows = 0;
+    int choice;
+    string line;
+    ifstream file("Data/employee_details.csv")
+
+    while (getline(file, line))
+    {
+        rows++;
+    }
+
+    if (loginEmployeeNum <= rows)
+    {
+        // possibly add "welcome (first and second name)"
+        VerifyPassword();
+    }
+    else
+    {
+        // "This employee number does not exist, enter 1 to try again or 2 to register for the annual leave service with IT"
+        if (choice==1)
+        {
+            Login()
+        }
+        else
+        {
+            //"Please contact IT on -------- to register an account"
+        }
+        
+    }
+
+}
+
+Login::VerifyPassword()
+{
+    cout << "/nEnter your Password" << endl;
+    cin << loginPassword;
+    cout << "/nLogin successful!" << endl;
+    // return employee number if successful
+    // return 0 if unsuccessful
+}
+
--- Login.h
+++ Login.h
@@ -3,3 +3,30 @@
  * @author Jack Pang (u1942995@live.warwick.ac.uk)
  * @brief
  */
+
+#ifndef Login_H
+#define Login_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+class Login // Add child of employees
+{
+public:
+    Login();
+
+private:
+    bool VerifyEmployee();
+    int VerifyPassword();
+    int loginEmployeeNum;
+    int databaseEmployeeNum;
+    string loginPassword;
+    string databasePassword;
+};
+
+#endif

37a373b121135f65b6b9d5305c6dcd1da3acba82
Henry <henryskiptoncarter@gmail.com>
2022-02-27 14:21:30 +0000

    Removed method for view multiple leave to avoid repeats

--- Employee.h
+++ Employee.h
@@ -38,7 +38,6 @@ public:
     void changeLeave();
     void listLeave(int empNum);
     vector<string> readData(string fileName, int empNum);
-    vector <vector<string> > readMultipleData(string fileName, int rowPos, int num);
     vector<string> editData(string fileName, string tempName, int empNum, int field, string newData);
     void writeData(string fileName, vector<string> data);
     void deleteData(string fileName, string tempFile, int empNum);

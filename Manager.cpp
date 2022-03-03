1864b97e0541a037ad973ffbb2504ae23da1070a
Scott Tomlins <scott.tomlins@dyson.com>
2022-03-03 19:01:30 +0000

    commented manager class

--- Manager.cpp
+++ Manager.cpp
@@ -9,42 +9,46 @@
 
 Manager::Manager(int num)
 {
+    // initialise variables
     employeeNumber = num;
     int option;
-    string emp_details = "Data/employee_details.csv";
     string emp_leave = "Data/employee_leave.csv";
     vector<vector<string> > all_leave;
 
+    // store all leave relevant to manager
     all_leave = readMultipleData(emp_leave, 1, employeeNumber);
 
+    // display options and store answer
     cout << "----- Manager Menu ----" << endl
          << "What would you like to do?" << endl
          << "(1) Review your employees leave" << endl
          << "(2) View your employees upcoming leave" << endl
          << "(3) Exit" << endl;
-
     cin >> option;
 
     switch (option)
     {
-    case 1:
-        reviewLeave(all_leave);
-        break;
-    case 2:
-        viewUpcomingLeave(all_leave);
-        break;
-    case 3:
-        break;
-    default:
-        cout << "Enter a value between 1 and 3" << endl;
+        case 1:
+            reviewLeave(all_leave);
+            break;
+        case 2:
+            viewUpcomingLeave(all_leave);
+            break;
+        case 3:
+            break;
+        default:
+            cout << "Enter a value between 1 and 3" << endl;
     }
 }
 
 void Manager::reviewLeave(vector<vector<string> > all_leave){
+    // initialise variables
     vector<int> leave_review;
     int leave_approve = 0;
+
     cout << "----- Leave requested by your employees -----" << endl;
 
+    // output all leave in review
     for (int i = 0; i < all_leave.size(); i++){
         if (all_leave[i][4] == "In Review"){
             leave_review.push_back(i+1);
@@ -55,10 +59,11 @@ void Manager::reviewLeave(vector<vector<string> > all_leave){
         }
     }
 
+    // ask manager what leave to approve
     cout << "Enter the number of any leave you want to APPROVE, if not enter 0" << endl;
     cin >> leave_approve;
-//    leave_approve--;
 
+    // update csv with "Approved" if number entered is a valid option
     if (count(leave_review.begin(), leave_review.end(), leave_approve)) {
         editData("Data/employee_leave.csv", "Data/temp.csv", employeeNumber, 1, 4, "Approved", leave_approve);
 
@@ -68,10 +73,11 @@ void Manager::reviewLeave(vector<vector<string> > all_leave){
         cout << "Leave approved!" << endl;
     }
 
+    // ask manager what leave to deny
     cout << "Enter the number of any leave you want to DENY, if not enter 0" << endl;
     cin >> leave_approve;
-//    leave_approve--;
 
+    // update csv with "Denied" if number entered is a valid option
     if (count(leave_review.begin(), leave_review.end(), leave_approve)) {
         editData("Data/employee_leave.csv", "Data/temp.csv", employeeNumber, 1, 4, "Denied", leave_approve);
 
@@ -86,6 +92,7 @@ void Manager::viewUpcomingLeave(vector<vector<string> > all_leave)
 {
     cout << "----- Upcoming leave for your employees -----" << endl;
 
+    // display all approved leave relevant to manager
     for (int i = 0; i < all_leave.size(); i++)
     {
         if (all_leave[i][4] == "Approved")
